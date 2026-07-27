12a4e2f977359580124390fa0159ddd2895a32a3
dde1270 <edmondodai2019@gmail.com>
2026-07-27 12:14:09 +0000

    git commit -m "No changes made to the code, just added comments to explain the code."

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -34,4 +34,21 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
+try:
+    user_input = int(input("Enter a number: "))
+
+    if is_prime(user_input):
+        print (f"{user_input} is a prime number.")
+    else:
+        print(f"{user_input} is NOT a prime number.")
+
+except ValueError: 
+    print("Please enter a valid integer.")
